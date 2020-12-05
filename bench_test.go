d5c22e955e24b830b702cb9d8d898ed4cd768629
reus <reusee@gmail.com>
2020-12-05 22:49:22 +0800

    add benchmarks

---
+++ bench_test.go
@@ -0,0 +1,33 @@
+package e4
+
+import (
+	"errors"
+	"io"
+	"testing"
+)
+
+var testErr error
+
+func BenchmarkCatchNil(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		err := func() (err error) {
+			defer Catch(&err)
+			return nil
+		}()
+		if err != nil {
+			b.Fatal()
+		}
+	}
+}
+
+func BenchmarkCatchErr(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		err := func() (err error) {
+			defer Catch(&err)
+			return io.EOF
+		}()
+		if !errors.Is(err, io.EOF) {
+			b.Fatal()
+		}
+	}
+}
