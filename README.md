1f0283fd1473b36abc107ba9128329b0192e2914
reus <reusee@gmail.com>
2021-01-19 16:14:33 +0800

    add copy file example

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # e4
 error utilities version 4
+
+### Example
+
+
+```
+func CopyFile(src, dst string) (err error) {
+  defer Handle(&err,
+    WithInfo("copy %s to %s", src, dst),
+  )
+
+  r, err := os.Open(src)
+  Check(err, WithInfo("open %s", src))
+  defer r.Close()
+
+  w, err := os.Create(dst)
+  Check(err, WithInfo("create %s", dst))
+  defer w.Close()
+  defer Handle(&err, WithFunc(func() {
+    os.Remove(dst)
+  }))
+
+  _, err = io.Copy(w, r)
+  Check(err)
+  Check(w.Close())
+
+  return
+}
+
+```
--- example_test.go
+++ example_test.go
@@ -136,3 +136,32 @@ func ExampleHandle_igonre() {
 	// Output:
 	// <nil>
 }
+
+func ExampleCheck_copyFile() {
+
+	copyFile := func(src, dst string) (err error) {
+		defer Handle(&err,
+			WithInfo("copy %s to %s", src, dst),
+		)
+
+		r, err := os.Open(src)
+		Check(err, WithInfo("open %s", src))
+		defer r.Close()
+
+		w, err := os.Create(dst)
+		Check(err, WithInfo("create %s", dst))
+		defer w.Close()
+		defer Handle(&err, WithFunc(func() {
+			os.Remove(dst)
+		}))
+
+		_, err = io.Copy(w, r)
+		Check(err)
+		Check(w.Close())
+
+		return
+	}
+
+	Check(copyFile("example_test.go", "example_test.go"))
+
+}
