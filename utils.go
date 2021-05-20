f79cfc4859de57a9963f7176e1e49c664f489911
reus <reusee@gmail.com>
2021-05-20 13:11:29 +0800

    add IgnoreAs, IgnoreContains

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package e4
 import (
 	"errors"
 	"io"
+	"strings"
 )
 
 func WithClose(c io.Closer) WrapFunc {
@@ -24,6 +25,24 @@ func WithFunc(fn func()) WrapFunc {
 func Ignore(err error) WrapFunc {
 	return func(prev error) error {
 		if errors.Is(prev, err) {
+			return nil
+		}
+		return prev
+	}
+}
+
+func IgnoreAs(target any) WrapFunc {
+	return func(prev error) error {
+		if errors.As(prev, target) {
+			return nil
+		}
+		return prev
+	}
+}
+
+func IgnoreContains(str string) WrapFunc {
+	return func(prev error) error {
+		if e := prev.Error(); strings.Contains(e, str) {
 			return nil
 		}
 		return prev
