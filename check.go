f8546d46989c0e5ce50e16a5969bbd3952afb77b
reus <reusee@gmail.com>
2021-05-20 10:04:54 +0800

    add comments

--- check.go
+++ check.go
@@ -35,6 +35,7 @@ func Must(err error, fns ...WrapFunc) {
 
 func Handle(errp *error, fns ...WrapFunc) {
 	var err error
+	// check throw error
 	if p := recover(); p != nil {
 		if e, ok := p.(*throw); ok {
 			err = e.err
@@ -42,22 +43,29 @@ func Handle(errp *error, fns ...WrapFunc) {
 			panic(p)
 		}
 	}
+	// check pointed error
 	if errp != nil && *errp != nil {
 		if err == nil {
+			// no throw error
 			err = *errp
 		} else {
+			// wrap if not the same
 			if !errors.Is(err, *errp) && !errors.Is(*errp, err) {
 				err = MakeErr(err, *errp)
 			}
 		}
 	}
 	if err == nil {
+		// no error
 		return
 	}
+	// wrap
 	err = Wrap(err, fns...)
 	if errp != nil {
+		// set pointed variable
 		*errp = err
 	} else {
+		// throw
 		Throw(err)
 	}
 }
